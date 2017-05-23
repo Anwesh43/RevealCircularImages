2277040a8b56542d3d7933c5ac82515183053543
Anwesh43 <anweshthecool0@gmail.com>
2017-05-23 08:34:31 +0530

    Using OnClickListener in app

--- app/src/main/java/com/anwesome/ui/circularrevealimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/circularrevealimagelistdemo/MainActivity.java
@@ -5,7 +5,9 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.anwesome.ui.revealcircularimagelist.OnSelectionChangeListener;
 import com.anwesome.ui.revealcircularimagelist.RCIList;
 import com.anwesome.ui.revealcircularimagelist.RevealCircularView;
 
@@ -17,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
         RCIList rciList = new RCIList(this);
         for(int i=0;i<6;i++) {
-            rciList.addImage(bitmap);
+            final int index = i+1;
+            rciList.addImage(bitmap, new OnSelectionChangeListener() {
+                @Override
+                public void onSelected() {
+                    Toast.makeText(MainActivity.this, index+" selected", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onUnSelected() {
+                    Toast.makeText(MainActivity.this, index+" unselected", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         rciList.show();
     }
