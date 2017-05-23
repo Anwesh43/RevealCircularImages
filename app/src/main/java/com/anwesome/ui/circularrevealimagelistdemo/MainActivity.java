effc4d59369bf90051faf7b3c3f2dc53c8131b6e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-23 08:30:34 +0530

    Using RevealCircularImageList in app

--- app/src/main/java/com/anwesome/ui/circularrevealimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/circularrevealimagelistdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+import com.anwesome.ui.revealcircularimagelist.RCIList;
 import com.anwesome.ui.revealcircularimagelist.RevealCircularView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
-        RevealCircularView revealCircularView = new RevealCircularView(this,bitmap);
-        addContentView(revealCircularView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,500));
+        RCIList rciList = new RCIList(this);
+        for(int i=0;i<6;i++) {
+            rciList.addImage(bitmap);
+        }
+        rciList.show();
     }
 }
